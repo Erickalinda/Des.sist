f886f135c3d8e0776a7e3d520d8ae0cb694475ca
Erickalinda <erickasmaniotto@gmail.com>
2025-02-21 09:30:52 -0300

    Add files via upload

---
+++ aula1.py
@@ -0,0 +1,11 @@
+import random
+sorteio = random.randint(1,10)
+print("### JOGO DA ADIVINHAÇÃO")
+print("Adivinhe o número que estou pensando, de 1 a 10")
+limite_tentativas = 3
+tentativa = 1 
+while (limite_tentativas >= tentativa):
+    print("Tentativa", tentativa)
+    chute = int(input("Digite o seu chute:"))
+    if (chute == sorteio):
+        print(parabéns )
